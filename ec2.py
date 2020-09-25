3ac841109a1499ac781af33a9229c3c041370403
dantso <35578467+dantso@users.noreply.github.com>
2020-09-25 09:46:36 -0400

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -5,11 +5,14 @@ import time
 
 ec2 = boto3.resource('ec2', region_name='us-east-1')
 instance = ec2.create_instances(
+    #change ImageId to your ImageId
     ImageId = 'ami-0c94855ba95c71c99',
     MinCount = 1,
     MaxCount = 1,
     InstanceType = 't2.micro',
+    #change Keyname to your KeyName
     KeyName = 'Ansible',
+    #change SecurityGroupIds to your SecurityGroupIds
     SecurityGroupIds=[
         'sg-0a946195e26dcdd80',
     ],
@@ -24,7 +27,9 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 client.load_system_host_keys()
+#change key_filename to the location where your key is stored
 client.connect(hostname=instance[0].public_ip_address, username="ec2-user", key_filename='C:/Users/Sonu_2/Downloads/Ansible.pem')
+#change git clone to your github repository url
 stdin, stdout, stderr = client.exec_command('sudo yum install git -y && git clone https://github.com/becloudready/aws-python.git && sudo bash ~/aws-python/flaskApp/shell.sh')
 print (stdout.readlines())
 time.sleep(3)
