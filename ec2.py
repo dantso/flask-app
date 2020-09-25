c4233b8f540f24538a2673c1f9d8661a6acc2c63
dantso <35578467+dantso@users.noreply.github.com>
2020-09-25 09:32:01 -0400

    Add files via upload

---
+++ ec2.py
@@ -0,0 +1,39 @@
+import boto3
+import botocore
+import paramiko
+import time
+
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+instance = ec2.create_instances(
+    ImageId = 'ami-0c94855ba95c71c99',
+    MinCount = 1,
+    MaxCount = 1,
+    InstanceType = 't2.micro',
+    KeyName = 'Ansible',
+    SecurityGroupIds=[
+        'sg-0a946195e26dcdd80',
+    ],
+)
+print (instance[0].id)
+instance[0].wait_until_running()           
+instance[0].reload()
+print (instance[0].public_ip_address)
+time.sleep(10)
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+client.load_system_host_keys()
+client.connect(hostname=instance[0].public_ip_address, username="ec2-user", key_filename='C:/Users/Sonu_2/Downloads/Ansible.pem')
+stdin, stdout, stderr = client.exec_command('sudo yum install git -y && git clone https://github.com/becloudready/aws-python.git && sudo bash ~/aws-python/flaskApp/shell.sh')
+print (stdout.readlines())
+time.sleep(3)
+stdin, stdout, stderr = client.exec_command('python ~/aws-python/flaskApp/app.py &')
+print (stdout.readlines())
+time.sleep(3)
+#client.close()
+
+print ("Finished")
+
+creatEC2()
+sshConnection()
---
+++ shell.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum update -y
+yum install python-pip -y;
+pip install flask;
